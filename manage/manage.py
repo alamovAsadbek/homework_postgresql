c9ffec255c073447557d10ab4744177c11085f4c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-05 14:15:22 +0500

    change create_company_table func in manager.py

--- manage/manage.py
+++ manage/manage.py
@@ -1,7 +1,16 @@
 from main_files.decorator.decorator_func import log_decorator
+from main_files.database.db_setting import execute_query
 
 
 class Manage:
     @log_decorator
     def create_company_table(self):
-        pass
+        query = '''
+        CREATE TABLE IF NOT EXISTS company (
+        ID BIGSERIAL PRIMARY KEY,
+        NAME VARCHAR(255) NOT NULL,
+        CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        )
+        '''
+        execute_query(query)
+        return True
