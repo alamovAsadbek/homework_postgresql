048643da0fd0b518311649312fa79a44e4ec9586
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-05 19:45:55 +0500

    change

--- requests.sql
+++ requests.sql
@@ -1,6 +1,23 @@
+-- Birinchi database yaratib oldim yani postgres databaseni uzgartirmaslik uchun
 CREATE DATABASE statistics_database;
-CREATE TABLE IF NOT EXISTS company (
-        ID BIGSERIAL PRIMARY KEY,
-        NAME VARCHAR(255) NOT NULL,
-        CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+
+-- Yangi table yasadim. Company table
+CREATE TABLE IF NOT EXISTS company
+(
+    ID         BIGSERIAL PRIMARY KEY,
+    NAME       VARCHAR(255) NOT NULL,
+    CREATED_AT TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Company tablega malumot qo'shib oldim
+INSERT INTO Company (name)
+VALUES ('Tech Innovators Inc.'),
+       ('Global Financial Services Ltd.'),
+       ('Wellness Solutions LLC'),
+       ('SuperMart'),
+       ('EduTech Systems'),
+       ('Fast Transport Co.'),
+       ('Prime Properties'),
+       ('Green Energy Solutions'),
+       ('Precision Manufacturing Corp.'),
+       ('Creative Media Group');
