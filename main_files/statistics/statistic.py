2b78e4c153f8a885c08adfdb7862baf614e6b2bf
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-05 21:43:33 +0500

    change

--- main_files/statistics/statistic.py
+++ main_files/statistics/statistic.py
@@ -17,12 +17,21 @@ class Statistic:
         LEFT JOIN employee e ON e.DEPARTMENT_ID = d.id
         GROUP BY c.name;
         '''
-        result = execute_query(query, fetch='all')
+        result_exc = execute_query(query, fetch='all')
         pagination = Pagination(table_name='users', table_keys=[0, 1], display_keys=['Name', 'Count'],
-                                data=result)
+                                data=result_exc)
         pagination.page_tab()
-        return result
+        return True
 
     @log_decorator
     def number_of_departments(self):
-        pass
+        query = '''
+        SELECT c.name as NAME, COUNT(c.ID) as All_DEPARTMENTS
+        FROM company c
+        LEFT JOIN department d ON d.COMPANY_ID = c.ID
+        GROUP BY c.name;
+        '''
+        result_exc = execute_query(query, fetch='all')
+        pagination = Pagination(table_name='users', table_keys=[0, 1], display_keys=[0, 1], data=result_exc)
+        pagination.page_tab()
+        return True
