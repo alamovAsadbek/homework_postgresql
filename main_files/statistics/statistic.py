5403ee52da49e15bd93ad43644ae06f2c142b2c7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-05 20:56:31 +0500

    change number_of_employees func

--- main_files/statistics/statistic.py
+++ main_files/statistics/statistic.py
@@ -1,7 +1,16 @@
 from main_files.decorator.decorator_func import log_decorator
+from main_files.database.db_setting import execute_query
 
 
 class Statistic:
     @log_decorator
     def number_of_employees(self):
-        pass
+        query = '''
+        SELECT c.name, COUNT(*)
+        FROM COMPANY AS c
+        LEFT JOIN DEPARTMENT AS d ON c.department_id = d.id
+        LEFT JOIN EMPLOYEE AS e ON e.DEPARTMENT_ID = d.id
+        GROUP BY c.name, COUNT(*)
+        '''
+        result = execute_query(query)
+        return result
