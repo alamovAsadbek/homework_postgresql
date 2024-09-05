4b1a2c8797c05e58d74c131e79259188a8c4d458
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-05 13:25:23 +0500

    change main_func func in main.py

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ from main_files.decorator.decorator_func import log_decorator
 
 @log_decorator
 def main_func():
-    pass
+    text = '''
+1. Number of employees
+    '''
+    print(text)
+    try:
+        pass
+    except Exception as e:
+        print(f'Error: {e}')
+        main_func()
